43ce38ade4f86c291c1519bfac049d021f3c0fa9
marcodems <marcodemos97@hotmail.it>
2025-11-03 09:09:04 +0100

    tabella creata e inseriti dei filtri lato client (da spostare lato server)

---
+++ SmsMachineDashboard/Models/CampaignFilters.cs
@@ -0,0 +1,9 @@
+﻿namespace SmsMachineDashboard.Models
+{
+    public class CampaignFilters
+    {
+        public string? Title { get; set; }
+        public DateTime? DateFrom { get; set; }
+        public DateTime? DateTo { get; set; }
+    }
+}
